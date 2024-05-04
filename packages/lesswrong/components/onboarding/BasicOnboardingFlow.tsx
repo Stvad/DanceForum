ac33c8934e90ab6b8b02f137bdffc407715a40e3
Vladyslav Sitalo <sitalov@amazon.com>
2024-05-04 12:39:13 -0700

    Generalizing the onboarding flow: review feedback

--- packages/lesswrong/components/onboarding/BasicOnboardingFlow.tsx
+++ packages/lesswrong/components/onboarding/BasicOnboardingFlow.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Components, registerComponent} from '../../lib/vulcan-lib'
 
-const BasicOnboardingFlow = () => <Components.OnboardingFlow stages={{
-  user: <Components.EAOnboardingUserStage icon={null}/>,
-  additionalInfo: <Components.DFAdditionalInfoStage/>,
-}} />
+const BasicOnboardingFlow = ({viewAsAdmin}: { viewAsAdmin?: boolean }) => <Components.OnboardingFlow
+  viewAsAdmin={viewAsAdmin}
+  stages={{
+    user: <Components.EAOnboardingUserStage icon={null}/>,
+    additionalInfo: <Components.DFAdditionalInfoStage/>,
+  }}/>
 
 const BasicOnboardingFlowComponent = registerComponent(
   'BasicOnboardingFlow',
