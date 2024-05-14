28992a7cb338a6782c0450e9a0e31345361fb9b6
Vladyslav Sitalo <sitalov@amazon.com>
2024-05-13 23:54:38 -0700

    Tweak description for better SEO

--- packages/lesswrong/lib/routes.ts
+++ packages/lesswrong/lib/routes.ts
@@ -1233,7 +1233,7 @@ const eaLwAfForumSpecificRoutes = forumSelect<Route[]>({
       name:'home',
       path:'/',
       componentName: 'DFHome',
-      description: "A forum & a wiki for discussions about West Coast Swing (Modern Swing). A place to share and develop your understanding of the dance, find WCS events near you, and connect with other dancers. A Wiki covering WCS concepts and techniques, tips for dance conventions and other WCS related topics.",
+      description: "A forum & a wiki for discussions about West Coast Swing (Modern Swing) dance. A place to share and develop your understanding of the dance, find WCS events near you, and connect with other dancers. A Wiki covering WCS concepts and techniques, tips for dance conventions and other WCS related topics.",
       enableResourcePrefetch: true,
       sunshineSidebar: true,
       hasLeftNavigationColumn: true,
