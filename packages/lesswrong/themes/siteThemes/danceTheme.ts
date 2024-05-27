7f782d71cc4b9b0ee644b3700b2908c771dcc826
Vladyslav Sitalo <sitalov@amazon.com>
2024-05-27 11:54:37 -0700

    EALoginPopover: don't display EA logo

--- packages/lesswrong/themes/siteThemes/danceTheme.ts
+++ packages/lesswrong/themes/siteThemes/danceTheme.ts
@@ -34,6 +34,11 @@ export const danceForumTheme: SiteThemeSpecification = {
             borderRadius: 6,
           },
         },
+        EALoginPopover: {
+          lightbulb: {
+            display: 'none',
+          },
+        },
       },
     }
   },
