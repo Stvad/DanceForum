e6fea5e0aced7baaf5a605bbd2180b4176e452ff
Vladyslav Sitalo <sitalov@amazon.com>
2024-05-31 15:23:21 -0700

    Make quickTakesMaxAgeDaysSetting into a setting #to-upstream

--- packages/lesswrong/components/quickTakes/QuickTakesSection.tsx
+++ packages/lesswrong/components/quickTakes/QuickTakesSection.tsx
@@ -11,6 +11,7 @@ import {
 import { isEAForum } from "../../lib/instanceSettings";
 import { isFriendlyUI, preferredHeadingCase } from "../../themes/forumTheme";
 import { Link } from '../../lib/reactRouterWrapper';
+import {quickTakesMaxAgeDaysSetting} from '../../lib/publicSettings'
 
 const styles = (theme: ThemeType) => ({
   communityToggle: {
@@ -120,6 +121,7 @@ const QuickTakesSection = ({classes}: {
       <QuickTakesList
         showCommunity={showCommunity}
         className={classes.list}
+        maxAgeDays={quickTakesMaxAgeDaysSetting.get()}
       />
     </ExpandableSection>
   );
